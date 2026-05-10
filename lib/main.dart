71e3f4071ab78814261ebac07d2a91892e3a369a
shreshthabagohil <shreshthabagohil@gmail.com>
2026-05-10 19:18:33 +0530

    feat: initial Flutter project structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,1 @@
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(
-    const MaterialApp(
-      home: Scaffold(
-        body: SizedBox.expand(),
-      ),
-    ),
-  );
-}
+void main() {}
